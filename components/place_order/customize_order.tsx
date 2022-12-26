7e3d6e3645b349f2ce15d3b7b9a720f6e24830a3
JonathanMov <jonathan@jointech-mx.com>
2022-12-26 13:33:21 -0800

    linter added

--- components/place_order/customize_order.tsx
+++ components/place_order/customize_order.tsx
@@ -1,16 +1,17 @@
-import { Step } from "pages/order"
-import { Dispatch, SetStateAction } from "react"
+import { Step } from 'pages/order'
+import { Dispatch, SetStateAction } from 'react'
 
 interface Props extends Plate {
-    triggerStep: Dispatch<SetStateAction<Step>>
-    setOrderInfo: Dispatch<SetStateAction<Order>>
+  triggerStep: Dispatch<SetStateAction<Step>>
+  setOrderInfo: Dispatch<SetStateAction<Order>>
 }
 
 export const CustomizeOrder = ({ triggerStep, setOrderInfo, ...plate }: Props) => {
-    const { name, price, description, image } = plate
-    return (
-        <div>
-            <h1>Customize Plate</h1>
-        </div>
-    )
+  // eslint-disable-next-line no-unused-vars
+  const { name, price, description, image } = plate
+  return (
+    <div>
+      <h1>Customize Plate</h1>
+    </div>
+  )
 }
