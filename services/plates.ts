2563ef7b71052395849325473a739fb14e756cce
JonathanMov <jonathan@jointech-mx.com>
2022-12-29 22:47:12 -0800

    menu endpoint changed

--- services/plates.ts
+++ services/plates.ts
@@ -4,7 +4,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localAPI_URL:8000'
 
 export const menus = {}
 
-export const getMenu = async () => (await axios.get(`${API_URL}/menu`)).data
+export const getMenu = async () => (await axios.get(`${API_URL}/plates`)).data
 
 export const createPlate = async (plateInfo: Plate) => (await axios.post(`${API_URL}/plates`, plateInfo, { withCredentials: true })).data
 
